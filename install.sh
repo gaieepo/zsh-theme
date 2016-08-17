bae0d0caba185b0b946114a03ea02750a9c381c5
Gaurav Manek <maneks@gmail.com>
2016-08-17 14:36:58 +0800

    Add a quick installer.

---
+++ install.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# Install zsh
+sudo apt-get install zsh git
+chsh -s /bin/zsh
+touch .histfile
+chmod u+rw .histfile
+
+# Install my .zshrc file and antigen
+# Move to the home directory
+cd ~
+RCFILE=".zshrc"
+ZGENREPO="https://github.com/tarjoilija/zgen.git"
+
+# Install the RC file
+if [ -e $RCFILE ]
+then
+	echo "$RCFILE found, skipping..."
+else
+	wget https://raw.githubusercontent.com/gauravmm/zsh-theme/master/.zshrc -O $RCFILE
+fi
+
+# Install zgen
+if [ -d $ZGENREPO ]
+then
+	echo "$ZGENREPO found, skipping..."
+else
+	git clone https://github.com/tarjoilija/zgen.git .zgen
+fi
+
+echo "Done! Starting zsh..."
+
+cd -
+zsh
