940bffc677fcf83361448eec1b824738f5f6f46f
Gaurav Manek <gaurav@gauravmanek.com>
2016-08-31 16:59:06 +0800

    Added HISTFILE change to installer.

--- install.sh
+++ install.sh
@@ -1,18 +1,32 @@
 #!/bin/sh
 
+RCFILE=".zshrc"
+ZGENREPO="https://github.com/tarjoilija/zgen.git"
+HISTFILE=".histfile"
+
+set -e
+
 # Install zsh
 sudo apt-get install zsh git
 chsh -s /bin/zsh
 
+#
 # Install my .zshrc file and antigen
+#
+
 # Move to the home directory
 cd ~
-touch .histfile
-chmod u+rw .histfile
-RCFILE=".zshrc"
-ZGENREPO="https://github.com/tarjoilija/zgen.git"
 
-# Install the RC file
+# Place the history file if it doesn't already exist.
+if [ -e $HISTFILE ]
+then
+	echo "$HISTFILE found, skipping..."
+else
+	touch $HISTFILE
+	chmod u+rw $HISTFILE
+fi
+
+# Install the RC file.
 if [ -e $RCFILE ]
 then
 	echo "$RCFILE found, skipping..."
@@ -20,7 +34,7 @@ else
 	wget https://raw.githubusercontent.com/gauravmm/zsh-theme/master/.zshrc -O $RCFILE
 fi
 
-# Install zgen
+# Install zgen.
 if [ -d $ZGENREPO ]
 then
 	echo "$ZGENREPO found, skipping..."
